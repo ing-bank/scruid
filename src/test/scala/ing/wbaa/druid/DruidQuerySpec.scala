8ad3e3061783959301c5664befc806b868371856
Toni Penya-Alba <toni.penya@adsmurai.com>
2018-07-26 10:40:15 +0200

    Add arithmetic post aggregations
    It also adds license to DruidQuerySpec' header since scalastyle's
    current configuration requires it.

--- src/test/scala/ing/wbaa/druid/DruidQuerySpec.scala
+++ src/test/scala/ing/wbaa/druid/DruidQuerySpec.scala
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package ing.wbaa.druid
 
 import org.scalatest._
@@ -16,6 +33,7 @@ class DruidQuerySpec extends WordSpec with Matchers with ScalaFutures {
   case class GroupByIsAnonymous(isAnonymous: String, count: Int)
   case class TopCountry(count: Int, countryName: Option[String])
   case class AggregatedFilteredAnonymous(count: Int, isAnonymous: String, filteredCount: Int)
+  case class PostAggregationAnonymous(count: Int, isAnonymous: String, halfCount: Double)
 
   "TimeSeriesQuery" should {
     "successfully be interpreted by Druid" in {
@@ -199,6 +217,42 @@ class DruidQuerySpec extends WordSpec with Matchers with ScalaFutures {
         topN(1) shouldBe AggregatedFilteredAnonymous(count = 3799,
                                                      filteredCount = 1556,
                                                      isAnonymous = "true")
+      }
+    }
+  }
+
+  "also work with post 'arithmetic' postaggregations" should {
+    "successfully be interpreted by Druid" in {
+      val request = TopNQuery(
+        dimension = Dimension(
+          dimension = "isAnonymous"
+        ),
+        threshold = 5,
+        metric = "count",
+        aggregations = List(
+          LongSumAggregation(name = "count", fieldName = "count")
+        ),
+        postAggregations = List(
+          ArithmeticPostAggregation(name = "halfCount",
+                                    fn = "/",
+                                    fields = (
+                                      FieldAccessPostAggregator(name = "count",
+                                                                fieldName = "count"),
+                                      ConstantPostAggregator(name = "two", value = 2)
+                                    ))
+        ),
+        intervals = List("2011-06-01/2017-06-01")
+      ).execute
+
+      whenReady(request) { response =>
+        val topN = response.list[PostAggregationAnonymous]
+        topN.size shouldBe 2
+        topN.head shouldBe PostAggregationAnonymous(count = 35445,
+                                                    halfCount = 17722.5,
+                                                    isAnonymous = "false")
+        topN(1) shouldBe PostAggregationAnonymous(count = 3799,
+                                                  halfCount = 1899.5,
+                                                  isAnonymous = "true")
       }
     }
   }
