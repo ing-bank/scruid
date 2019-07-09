7e13d13bfaba11a1f6d64e1db28907a9ec6ec97d
Anastasios Skarlatidis <a.skarlatidis@gmail.com>
2019-07-09 23:03:14 +0300

    Query context support
      - Context params is a Map
      - Parameters for all query types
      - Builder for DQL

---
+++ src/main/scala/ing/wbaa/druid/definitions/QueryContext.scala
@@ -0,0 +1,69 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ing.wbaa.druid.definitions
+
+object QueryContext {
+
+  type QueryContextParam = String
+  type QueryContextValue = String
+
+  // the following apply only to all queries
+  final val Timeout                      = "timeout"
+  final val Priority                     = "priority"
+  final val QueryId                      = "queryId"
+  final val UseCache                     = "useCache"
+  final val PopulateCache                = "populateCache"
+  final val UseResultLevelCache          = "useResultLevelCache"
+  final val PopulateResultLevelCache     = "populateResultLevelCache"
+  final val BySegment                    = "bySegment"
+  final val Finalize                     = "finalize"
+  final val ChunkPeriod                  = "chunkPeriod"
+  final val MaxScatterGatherBytes        = "maxScatterGatherBytes"
+  final val MaxQueuedBytes               = "maxQueuedBytes"
+  final val SerializeDateTimeAsLong      = "serializeDateTimeAsLong"
+  final val SerializeDateTimeAsLongInner = "serializeDateTimeAsLongInner"
+
+  // the following apply only to time-series queries
+  final val SkipEmptyBuckets = "skipEmptyBuckets"
+  final val GrandTotal       = "grandTotal"
+
+  // the following apply only to top-n queries
+  final val MinTopNThreshold = "minTopNThreshold"
+
+  // the following apply only to group-by queries
+  final val GroupByStrategy         = "groupByStrategy"
+  final val GroupByIsSingleThreaded = "groupByIsSingleThreaded"
+
+  // group-by V2 parameters
+  final val MaxMergingDictionarySize    = "maxMergingDictionarySize"
+  final val MaxOnDiskStorage            = "maxOnDiskStorage"
+  final val BufferGrouperInitialBuckets = "bufferGrouperInitialBuckets"
+  final val BufferGrouperMaxLoadFactor  = "bufferGrouperMaxLoadFactor"
+  final val ForceHashAggregation        = "forceHashAggregation"
+  final val IntermediateCombineDegree   = "intermediateCombineDegree"
+  final val NumParallelCombineThreads   = "numParallelCombineThreads"
+
+  final val SortByDimsFirst    = "sortByDimsFirst"
+  final val ForceLimitPushDown = "forceLimitPushDown"
+
+  // group-by V1 parameters
+  final val MaxIntermediateRows = "maxIntermediateRows"
+  final val MaxResults          = "maxResults"
+  final val UseOffheap          = "useOffheap"
+
+}
---
+++ src/test/scala/ing/wbaa/druid/QueryContextSpec.scala
@@ -0,0 +1,69 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ing.wbaa.druid
+
+import ing.wbaa.druid.definitions._
+import io.circe.generic.auto._
+import io.circe.syntax._
+import org.scalatest._
+import org.scalatest.concurrent._
+import org.scalatest.time._
+
+class QueryContextSpec extends WordSpec with Matchers with ScalaFutures {
+
+  implicit override val patienceConfig =
+    PatienceConfig(timeout = Span(20, Seconds), interval = Span(5, Millis))
+  private val totalNumberOfEntries = 39244
+  implicit val config              = DruidConfig()
+  implicit val mat                 = config.client.actorMaterializer
+
+  case class TimeseriesCount(count: Int)
+
+  "TimeSeriesQuery with context" should {
+
+    "successfully be interpreted by Druid" in {
+
+      val query = TimeSeriesQuery(
+        aggregations = List(
+          CountAggregation(name = "count")
+        ),
+        granularity = GranularityType.Hour,
+        intervals = List("2011-06-01/2017-06-01"),
+        context = Map(
+          QueryContext.QueryId          -> "some_custom_id",
+          QueryContext.Priority         -> "100",
+          QueryContext.UseCache         -> "false",
+          QueryContext.SkipEmptyBuckets -> "true"
+        )
+      )
+
+      val requestJson = query.asJson.noSpaces
+
+      requestJson shouldBe
+      """{"aggregations":[{"name":"count","type":"count"}],"intervals":["2011-06-01/2017-06-01"],"filter":null,"granularity":"hour","descending":"true","postAggregations":[],"context":{"queryId":"some_custom_id","priority":"100","useCache":"false","skipEmptyBuckets":"true"}}"""
+
+      val resultF = query.execute()
+
+      whenReady(resultF) { response =>
+        response.list[TimeseriesCount].map(_.count).sum shouldBe totalNumberOfEntries
+      }
+    }
+
+  }
+
+}
